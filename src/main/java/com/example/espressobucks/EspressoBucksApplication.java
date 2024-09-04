e6b5502b722bbb71393a8fdac0178f4f1a002466
lucasmodin <lumo0002@stud.kea.dk>
2024-09-05 01:03:36 +0200

    initial commit

---
+++ src/main/java/com/example/espressobucks/EspressoBucksApplication.java
@@ -0,0 +1,13 @@
+package com.example.espressobucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspressoBucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspressoBucksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/espressobucks/EspressoBucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.espressobucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspressoBucksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
